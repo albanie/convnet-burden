eb148e7120ea4c6ed0d03efef13742227a6a569a
albanie <albanie@robots.ox.ac.uk>
2017-08-04 11:12:00 +0100

    first commit

---
+++ setup_mcnBurden.m
@@ -0,0 +1,7 @@
+function setup_mcnBurden()
+%SETUP_MCNBURDEN Sets up mcnBURDEN, by adding its folders 
+% to the Matlab path, as well as setting up mcnFasterBURDEN as a dependency
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root) ;
+  addpath(root, [root '/matlab'], [root '/misc'], [root '/core']) ;
