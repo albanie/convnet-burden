2a9d6250bc24e0e4c59ae5b5c41d17decf069bb8
albanie <albanie@robots.ox.ac.uk>
2017-08-06 16:49:03 +0100

    auto-generation + licence

--- setup_convnet_burden.m
+++ setup_convnet_burden.m
@@ -1,6 +1,9 @@
 function setup_convnet_burden()
 %SETUP_CONVNET_BURDEN Sets up convnet-burden, by adding its folders 
 % to the Matlab path
+%
+% Copyright (C) 2017 Samuel Albanie 
+% Licensed under The MIT License [see LICENSE.md for details]
 
   root = fileparts(mfilename('fullpath')) ;
   addpath(root, [root '/matlab'], [root '/core']) ;
